fbdb27ee7f240cdb4dd3be8ccc6569b17f2ae8d7
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2020-11-01 19:47:35 -0600

    Added django-heroku

--- sloth_blog/settings.py
+++ sloth_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +129,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
