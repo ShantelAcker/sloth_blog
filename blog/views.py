55e0074da28b06347c7cd2230427f62e68ffa676
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2020-11-23 13:50:33 -0600

    Changed post ordering.

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .models import Post
 
 def index(request):
     """The home page of the blog that lists all posts."""
-    posts = Post.objects.order_by('pub_date')
+    posts = Post.objects.order_by('-pub_date')
     context = {'posts': posts}
     return render(request, 'blog/index.html', context)
 
